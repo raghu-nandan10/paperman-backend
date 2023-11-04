7b4116de73af810c5d41973283c4693ef6e7dd01
raghu-nandan10 <trafalgardwaterlaw393@gmail.com>
2023-11-04 18:11:34 +0530

    works

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL, "https://paperman-frontend.netlify.app"],
     credentials: true,
   })
 );
